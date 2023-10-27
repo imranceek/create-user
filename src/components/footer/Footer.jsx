cfc36c397138c36587fd13ec4f702e79e5dee5d6
anvarovimrobek <anvarovimrobek@gmail.com>
2023-10-27 22:59:16 +0500

    CUser full completed

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container">
+        <p>All rights reserved.</p>
+        <a href="#">Anvarov Imronbek</a>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
